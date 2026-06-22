d4a5616d65adf28d0b7e117fc82b4e227321b439
cesarstati <cesar@gmail.com>
2026-06-22 07:28:57 -0300

    Campos do Login

---
+++ src/components/Botao.jsx
@@ -0,0 +1,7 @@
+function Botao(){
+    return (
+        <button>Enviar</button>
+    )
+}
+
+export default Botao
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,19 @@
+import Botao from "./Botao"
+import './Login.css'
+
 function Login(){
     return (
         <>
+        <section className="container">
+            <h2>Login</h2>
+            <form action="" className='formulario'>
+                <input type="text" placeholder='Usuário'/>
+                <input type="password" placeholder='Senha'/>
+            </form>
+            <Botao />
 
-        <h2>Login</h2>
+        </section>
+        
         
         </>
         
