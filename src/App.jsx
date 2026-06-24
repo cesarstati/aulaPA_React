1c99064ddc1ffb3d1b6dd13d80bcf17620dd389b
cesarstati <cesar@gmail.com>
2026-06-24 11:37:54 -0300

    criacao de CardPro e Funcionario

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import CardProd from "./components/CardProd";
 import Header from "./components/Header";
 import Login from "./components/Login";
+import Funcionario from "./components/Funcionarios";
 
 function App(){
   return (
@@ -9,6 +11,20 @@ function App(){
             subtitulo = "O melhor da Região" />
     <Login />
 
+    <CardProd nome = "X-Burguer"
+              preco= "15,90"
+              />
+
+    <CardProd nome = "X-Salada"
+              preco= "17,90"
+              />
+
+    <CardProd nome = "Refrigerante"
+              preco= "6,00"
+              />
+
+    <Funcionario nome = "João"
+cargo = "Aendente" />
     </>
     
   )
---
+++ src/components/CardProd.jsx
@@ -0,0 +1,9 @@
+function CardProd({nome,preco}){
+    return (
+        <div>
+            <h3>{nome}</h3>
+            <p>R$ {preco}</p>
+        </div>
+    )
+}
+export default CardProd
